07e7e23ac80d1ccd7b1f3cae6831af80061a11c7
GiggZ34 <guillaume.rivalan@gmail.com>
2021-03-07 13:09:52 +0100

    update cross plateform

--- cub3d/includes/control_mac.h
+++ cub3d/includes/control_mac.h
@@ -6,16 +6,16 @@
 /*   By: grivalan <grivalan@studen.42lyon.fr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/06 23:45:16 by grivalan          #+#    #+#             */
-/*   Updated: 2021/03/07 00:11:32 by grivalan         ###   ########lyon.fr   */
+/*   Updated: 2021/03/07 13:06:22 by grivalan         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #define DESTROY 53
-#define DOWN 125
-#define UP 126
+#define UP 125
+#define DOWN 126
 #define RIGHT 123
 #define LEFT 124
-#define W 0
-#define S 2
-#define A 13
-#define D 1
+#define W 13
+#define S 1
+#define A 0
+#define D 2
